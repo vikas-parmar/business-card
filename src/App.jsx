006b9d6b6d2d99992132300e972a13860dbb375c
Vikas Panwar <vkparmar003@gmail.com>
2023-03-16 15:47:45 +0530

    git init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Components/Header';
+import MainContent from './Components/MainContent';
+import Footer from './Components/Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+};
+
+export default App;
