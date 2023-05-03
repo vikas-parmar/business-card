3d256acec66b2baf42cdd4c91a9b9f1160349ef5
Vikas Parmar <122597785+vikas-parmar@users.noreply.github.com>
2023-05-03 15:56:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import github from './Assets/githubLogo.svg'
 
 const bodyStyle = {
   minHeight: "100vh",
+  maxWidth="100vw",
   display: "flex",
   backgroundColor: "secondary.main",
   flexDirection: "column",
