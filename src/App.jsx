c83e06134a7666ee5bef53a55556ff2f2584610c
Vikas Panwar <vkparmar003@gmail.com>
2023-05-28 12:21:22 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { Container, Link } from '@mui/material';
+import { Container, Hidden, Link } from '@mui/material';
 import Header from './Components/Header';
 import MainContent from './Components/MainContent';
 import Footer from './Components/Footer';
-import github from './Assets/githubLogo.svg'
+import github from './Assets/githubLogo.svg';
 
 const bodyStyle = {
-  minHeight: "100vh",
-  minWidth: "100%",
   display: "flex",
+  minHeight: "100vh",
   backgroundColor: "secondary.main",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
 };
 
-const logoStyle = { position: "fixed", right: "2vw", top: "88vh" }
+const logoStyle = { position: "fixed", right: "2%", top: "90%" }
 
 const App = () => {
   return (
-    <Container minWidth="xl" sx={bodyStyle} >
+    <Container maxWidth="xl" sx={bodyStyle} >
       <Header />
       <MainContent />
       <Footer />
-      <Link href="https://github.com/vikas-parmar" underline="none" target="_blank" rel="noopener">
-        <img src={github} width={'40px'} style={logoStyle} alt='gitHub Logo' />
-      </Link>
+      <Hidden smDown>
+        <Link href="https://github.com/vikas-parmar" underline="none" target="_blank" rel="noopener">
+          <img src={github} width={'40px'} style={logoStyle} alt='gitHub Logo' />
+        </Link>
+      </Hidden>
     </Container>
   )
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { theme } from './theme'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
